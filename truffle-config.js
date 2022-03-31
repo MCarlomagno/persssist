910f9b404df4b701428b7ce9c11fa04e7c821dfc
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-03-31 17:11:44 -0300

    ci: setup secrets

--- truffle-config.js
+++ truffle-config.js
@@ -18,10 +18,6 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
 require('dotenv').config()
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const infuraProjectId = process.env.INFURA_PROJECT_ID
